247af050d7d60b7bb34c4e22c473479632a7394a
Andrew Demidow <ander.demid@mail.ru>
2024-12-10 03:13:36 +0300

    правки к проедланной работе(изменены докстринги и наименования первой функции)

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,19 @@
 
 
-def filter_by_state(list_of_dictionary: list, user_state: str = "EXECUTED") -> list:
-    """Функция выводящая список словарей с указанным значением для ключа state (по умолчанию значени EXECUTED)"""
-    new_list_dictionary = []
-    for dic in list_of_dictionary:
+def filter_by_state(list_of_dictionaries: list, user_state: str = "EXECUTED") -> list:
+    """Функция получающая на вход список словарей и дополнительный параметр(по умолчанию EXECUTED). Функция возвращает
+    список с теми словарями
+    в которых значение ключа state равнялось заданному параметру (по умолчанию значение EXECUTED)"""
+    new_list_of_dictionaries = []
+    for dic in list_of_dictionaries:
         if dic.get("state") == user_state:
-            new_list_dictionary.append(dic)
-    return new_list_dictionary
+            new_list_of_dictionaries.append(dic)
+    return new_list_of_dictionaries
 
 
 def sort_by_date(list_of_dates: list, is_decreasing: bool = True) -> list:
-    """Функция сортирующая список словарей по дате (по убыванию или по увеличению)
-    по умолчанию сортирует по убыванию)"""
+    """Функция получающая на вход список словарей и дополнительный параметр представляющий собой булевую переменную.
+    Функция сортирует список по датам (ключ "date")
+    и возвращает новый список словарей
+    с отсортированными по убыванию или возрастанию датами (По умолчанию сортирует по убыванию)"""
     return sorted(list_of_dates, key=lambda date_sorted: date_sorted["date"], reverse=is_decreasing)
