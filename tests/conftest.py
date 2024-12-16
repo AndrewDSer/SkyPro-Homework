6c948492a30ae059e27e5e9d2d3eb870c865eb77
Andrew Demidow <ander.demid@mail.ru>
2024-12-16 13:21:16 +0300

    Прописаны тесты для модуля widget

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,13 @@ def card_number():
 @pytest.fixture
 def account_number():
     return "34567291846572364752"
+
+
+@pytest.fixture
+def card_account():
+    return "Visa Platinum 7000792289606361"
+
+
+@pytest.fixture
+def date():
+    return "2024-03-11T02:26:18.671407"
