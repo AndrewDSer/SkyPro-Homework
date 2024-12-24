96573f11a7904caa56352f6dfd30606b7373edd2
Andrew Demidow <ander.demid@mail.ru>
2024-12-24 10:39:28 +0300

    прописаны тесты для нового модуля generators

--- src/generators.py
+++ src/generators.py
@@ -1,6 +1,8 @@
 
 
 def filter_by_currency(transactions, currency="USD"):
+    """Функция получающая на вход список словарей и возвращающая иттератор
+    который поочередно выдаёт  транзакции из изначального списка по заданной валюте"""
     for transaction in transactions:
         if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield transaction
@@ -9,6 +11,8 @@ def filter_by_currency(transactions, currency="USD"):
 
 
 def transaction_descriptions(transactions):
+    """Функция генератор которая принимает список словарей с транзакциями и
+    возвращает описание каждой операции по очереди"""
     for transaction in transactions:
         if transaction.get("description") == "":
             yield "Описание транзакции или транзакция отсутствует"
@@ -16,8 +20,10 @@ def transaction_descriptions(transactions):
             yield transaction.get("description")
 
 
-def card_number_generator(start, end):
-    for num in range(start, end):
+def card_number_generator(start, stop):
+    """Функция генератор которая принимает на вход начальное и конечное значение диапозона.
+    Функция генерирует номера карт в заданном диапозоне"""
+    for num in range(start, stop + 1):
         num_str = str(num)
         correct_card_num = 16 - len(num_str)
         generate_card_number = "0" * correct_card_num + num_str
